54dd1028e042adb5c6ed1917ce129659c147ded7
Lwgfangz <lwgfangz@fenda-newtechtekiiMac.local>
2013-05-30 10:09:13 +0800

    Initial Commit

---
+++ demo_bitmap/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  demo_bitmap
+//
+//  Created by Lwgfangz on 13-5-30.
+//  Copyright (c) 2013年 Lwgfangz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ demo_bitmap/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  demo_bitmap
+//
+//  Created by Lwgfangz on 13-5-30.
+//  Copyright (c) 2013年 Lwgfangz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
